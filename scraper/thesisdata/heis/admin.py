35343c8f297e020fa8a9d156ddde5e0079e89cc2
erynk <42783392+ekelseya@users.noreply.github.com>
2022-02-05 21:08:05 -0700

    Add models to admin.py

--- scraper/thesisdata/heis/admin.py
+++ scraper/thesisdata/heis/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Region, Institution, Page
 # Register your models here.
+
+admin.site.register(Region)
+admin.site.register(Institution)
+admin.site.register(Page)
